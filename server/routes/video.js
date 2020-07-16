718dfd66fad29909e8eea11380c523214a6c2fd2
정원형 <spowide@jeong-wonhyeong-ui-MacBookPro.local>
2020-07-16 18:44:22 +0900

    비디오 업로드 및 multer 사용

--- server/routes/video.js
+++ server/routes/video.js
@@ -37,7 +37,7 @@ router.post("/uploadfiles", (req, res) => {
         if (err) {
             return res.json({ success: false, err })
         }
-        return res.json({ success: true, filePath: res.req.file.path, fileName: res.req.file.filename })
+        return res.json({ success: true, url: res.req.file.path, fileName: res.req.file.filename })
     })
 
 });
@@ -45,10 +45,10 @@ router.post("/uploadfiles", (req, res) => {
 
 router.post("/thumbnail", (req, res) => {
 
-    let thumbsFilePath ="";
-    let fileDuration ="";
+    let thumbsFilePath = "";
+    let fileDuration = "";
 
-    ffmpeg.ffprobe(req.body.filePath, function(err, metadata){
+    ffmpeg.ffprobe(req.body.filePath, function (err, metadata) {
         console.dir(metadata);
         console.log(metadata.format.duration);
 
@@ -63,15 +63,15 @@ router.post("/thumbnail", (req, res) => {
         })
         .on('end', function () {
             console.log('Screenshots taken');
-            return res.json({ success: true, thumbsFilePath: thumbsFilePath, fileDuration: fileDuration})
+            return res.json({ success: true, thumbsFilePath: thumbsFilePath, fileDuration: fileDuration })
         })
         .screenshots({
             // Will take screens at 20%, 40%, 60% and 80% of the video
             count: 3,
             folder: 'uploads/thumbnails',
-            size:'320x240',
+            size: '320x240',
             // %b input basename ( filename w/o extension )
-            filename:'thumbnail-%b.png'
+            filename: 'thumbnail-%b.png'
         });
 
 });
@@ -84,7 +84,7 @@ router.get("/getVideos", (req, res) => {
     Video.find()
         .populate('writer')
         .exec((err, videos) => {
-            if(err) return res.status(400).send(err);
+            if (err) return res.status(400).send(err);
             res.status(200).json({ success: true, videos })
         })
 
@@ -97,9 +97,9 @@ router.post("/uploadVideo", (req, res) => {
     const video = new Video(req.body)
 
     video.save((err, video) => {
-        if(err) return res.status(400).json({ success: false, err })
+        if (err) return res.status(400).json({ success: false, err })
         return res.status(200).json({
-            success: true 
+            success: true
         })
     })
 
@@ -108,12 +108,12 @@ router.post("/uploadVideo", (req, res) => {
 
 router.post("/getVideo", (req, res) => {
 
-    Video.findOne({ "_id" : req.body.videoId })
-    .populate('writer')
-    .exec((err, video) => {
-        if(err) return res.status(400).send(err);
-        res.status(200).json({ success: true, video })
-    })
+    Video.findOne({ "_id": req.body.videoId })
+        .populate('writer')
+        .exec((err, video) => {
+            if (err) return res.status(400).send(err);
+            res.status(200).json({ success: true, video })
+        })
 });
 
 
@@ -121,26 +121,26 @@ router.post("/getSubscriptionVideos", (req, res) => {
 
 
     //Need to find all of the Users that I am subscribing to From Subscriber Collection 
-    
+
     Subscriber.find({ 'userFrom': req.body.userFrom })
-    .exec((err, subscribers)=> {
-        if(err) return res.status(400).send(err);
+        .exec((err, subscribers) => {
+            if (err) return res.status(400).send(err);
 
-        let subscribedUser = [];
+            let subscribedUser = [];
 
-        subscribers.map((subscriber, i)=> {
-            subscribedUser.push(subscriber.userTo)
-        })
-
-
-        //Need to Fetch all of the Videos that belong to the Users that I found in previous step. 
-        Video.find({ writer: { $in: subscribedUser }})
-            .populate('writer')
-            .exec((err, videos) => {
-                if(err) return res.status(400).send(err);
-                res.status(200).json({ success: true, videos })
+            subscribers.map((subscriber, i) => {
+                subscribedUser.push(subscriber.userTo)
             })
-    })
+
+
+            //Need to Fetch all of the Videos that belong to the Users that I found in previous step. 
+            Video.find({ writer: { $in: subscribedUser } })
+                .populate('writer')
+                .exec((err, videos) => {
+                    if (err) return res.status(400).send(err);
+                    res.status(200).json({ success: true, videos })
+                })
+        })
 });
 
 module.exports = router;
