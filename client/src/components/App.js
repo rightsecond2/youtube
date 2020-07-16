ece1dd44ec5607521263189c9e0a65bc30f00713
정원형 <spowide@jeong-wonhyeong-ui-MacBookPro.local>
2020-07-16 12:00:17 +0900

    youtube start

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -7,9 +7,7 @@ import LoginPage from "./views/LoginPage/LoginPage.js";
 import RegisterPage from "./views/RegisterPage/RegisterPage.js";
 import NavBar from "./views/NavBar/NavBar";
 import Footer from "./views/Footer/Footer"
-import UploadVideoPage from "./views/UploadVideoPage/UploadVideoPage"
-import DetailVideoPage from "./views/DetailVideoPage/DetailVideoPage"
-import SubscriptionPage from "./views/SubscriptionPage/SubscriptionPage"
+
 function App() {
   return (
     <Suspense fallback={(<div>Loading...</div>)}>
@@ -19,9 +17,6 @@ function App() {
           <Route exact path="/" component={Auth(LandingPage, null)} />
           <Route exact path="/login" component={Auth(LoginPage, false)} />
           <Route exact path="/register" component={Auth(RegisterPage, false)} />
-          <Route exact path="/video/upload" component={Auth(UploadVideoPage, true)} />
-          <Route exact path="/video/:videoId" component={Auth(DetailVideoPage, null)} />
-          <Route exact path="/subscription" component={Auth(SubscriptionPage, null)} />
         </Switch>
       </div>
       <Footer />
